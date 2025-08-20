7c954009548a3585340e0c0e36122429fc455ab1
wallenjos01 <wallenjos01@gmail.com>
2025-08-19 21:31:35 -0600

    Make MidnightProxy plugin send tag urls instead of resolving once

--- midnightproxy/src/main/java/org/wallentines/packserver/PackEntry.java
+++ midnightproxy/src/main/java/org/wallentines/packserver/PackEntry.java
@@ -1,5 +1,8 @@
 package org.wallentines.packserver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.UUID;
 import org.jetbrains.annotations.Nullable;
 import org.wallentines.mdcfg.serializer.InlineSerializer;
 import org.wallentines.mdcfg.serializer.ObjectSerializer;
@@ -8,80 +11,55 @@ import org.wallentines.mdproxy.ResourcePack;
 import org.wallentines.pseudonym.text.Component;
 import org.wallentines.pseudonym.text.ConfigTextParser;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-
-
-public record PackEntry(UUID uuid, @Nullable String server, @Nullable String tag, @Nullable String hash, @Nullable Component prompt, boolean required, @Nullable Component kickMessage) {
+public record PackEntry(UUID uuid, @Nullable String server,
+                        @Nullable String tag, @Nullable String hash,
+                        @Nullable Component prompt, boolean required,
+                        @Nullable Component kickMessage) {
 
     public PackEntry(String tag) {
-        this(UUID.nameUUIDFromBytes(tag.getBytes()), null, tag, null, null, false, null);
+        this(UUID.nameUUIDFromBytes(tag.getBytes()), null, tag, null, null,
+             false, null);
     }
 
-    private String getPackHash(HttpClient client, URI baseUrl) throws IOException, InterruptedException {
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(baseUrl.resolve("hash?tag=" + tag))
-                .build();
-
-        HttpResponse<byte[]> res = client.send(request, responseInfo -> HttpResponse.BodySubscribers.ofByteArray());
-        return new String(res.body());
-    }
-
-    public CompletableFuture<ResourcePack> toPack(PackServerPlugin plugin, Executor executor) {
+    public ResourcePack toPack(PackServerPlugin plugin) {
 
         URI baseUrl = plugin.getServer(server);
-        if(baseUrl == null) {
-            return CompletableFuture.completedFuture(null);
+        if (baseUrl == null) {
+            return null;
+        }
+        if (tag == null && hash == null) {
+            return null;
         }
 
-        if(hash != null) {
-            try {
-                return CompletableFuture.completedFuture(new ResourcePack(uuid, baseUrl.resolve("pack?hash=" + hash).toURL().toString(), hash, required, prompt));
-            } catch (MalformedURLException ex) {
-                throw new RuntimeException(ex);
-            }
+        URI finalUri = baseUrl.resolve("pack?" + tag == null ? ("hash=" + hash)
+                                                             : ("tag=" + tag));
+
+        try {
+            return new ResourcePack(uuid, finalUri.toURL().toString(), hash,
+                                    required, prompt);
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException(ex);
         }
-
-        return CompletableFuture.supplyAsync(() -> {
-            String packHash;
-            try(HttpClient client = HttpClient.newBuilder().executor(executor).build()) {
-                packHash = getPackHash(client, baseUrl);
-            } catch (IOException | InterruptedException ex) {
-                throw new RuntimeException(ex);
-            }
-
-            try {
-                return new ResourcePack(uuid, baseUrl.resolve("pack?hash=" + packHash).toURL().toString(), packHash, required, prompt);
-            } catch (MalformedURLException ex) {
-                throw new RuntimeException(ex);
-            }
-
-        }, executor);
-
     }
 
-    private static final Serializer<Component> COMPONENT_SERIALIZER = InlineSerializer.of(ConfigTextParser.INSTANCE::serialize, ConfigTextParser.INSTANCE::parse);
+    private static final Serializer<Component> COMPONENT_SERIALIZER =
+        InlineSerializer.of(ConfigTextParser.INSTANCE::serialize,
+                            ConfigTextParser.INSTANCE::parse);
 
-    public static final Serializer<PackEntry> SERIALIZER = ObjectSerializer.create(
-            Serializer.UUID.entry("uuid", PackEntry::uuid),
-            Serializer.STRING.entry("server", PackEntry::server).optional(),
-            Serializer.STRING.entry("tag", PackEntry::server).optional(),
-            Serializer.STRING.entry("hash", PackEntry::server).optional(),
-            COMPONENT_SERIALIZER.entry("prompt", PackEntry::prompt).optional(),
-            Serializer.BOOLEAN.entry("required", PackEntry::required).orElse(false),
-            COMPONENT_SERIALIZER.entry("kick_message", PackEntry::kickMessage).optional(),
-            PackEntry::new
-    ).or(Serializer.STRING.flatMap(PackEntry::tag, PackEntry::new));
-
-
-
+    public static final Serializer<PackEntry> SERIALIZER =
+        ObjectSerializer
+            .create(
+                Serializer.UUID.entry("uuid", PackEntry::uuid),
+                Serializer.STRING.entry("server", PackEntry::server).optional(),
+                Serializer.STRING.entry("tag", PackEntry::server).optional(),
+                Serializer.STRING.entry("hash", PackEntry::server).optional(),
+                COMPONENT_SERIALIZER.entry("prompt", PackEntry::prompt)
+                    .optional(),
+                Serializer.BOOLEAN.entry("required", PackEntry::required)
+                    .orElse(false),
+                COMPONENT_SERIALIZER
+                    .entry("kick_message", PackEntry::kickMessage)
+                    .optional(),
+                PackEntry::new)
+            .or(Serializer.STRING.flatMap(PackEntry::tag, PackEntry::new));
 }
--- midnightproxy/src/main/java/org/wallentines/packserver/PackServerPlugin.java
+++ midnightproxy/src/main/java/org/wallentines/packserver/PackServerPlugin.java
@@ -1,5 +1,11 @@
 package org.wallentines.packserver;
 
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.*;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +13,7 @@ import org.wallentines.mdcfg.ConfigList;
 import org.wallentines.mdcfg.ConfigObject;
 import org.wallentines.mdcfg.ConfigSection;
 import org.wallentines.mdcfg.codec.FileWrapper;
+import org.wallentines.mdcfg.registry.Registry;
 import org.wallentines.mdcfg.serializer.ConfigContext;
 import org.wallentines.mdcfg.serializer.InlineSerializer;
 import org.wallentines.mdcfg.serializer.Serializer;
@@ -16,30 +23,28 @@ import org.wallentines.mdproxy.ResourcePack;
 import org.wallentines.mdproxy.packet.ServerboundHandshakePacket;
 import org.wallentines.mdproxy.packet.common.ServerboundResourcePackStatusPacket;
 import org.wallentines.mdproxy.plugin.Plugin;
-import org.wallentines.mdcfg.registry.Registry;
 import org.wallentines.pseudonym.text.Component;
 import org.wallentines.pseudonym.text.Content;
 import org.wallentines.pseudonym.text.ImmutableComponent;
 import org.wallentines.pseudonym.text.Style;
 
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
-import java.util.concurrent.*;
-
 public class PackServerPlugin implements Plugin {
 
-
-    private static final ConfigSection DEFAULT_CONFIG = new ConfigSection()
+    private static final ConfigSection DEFAULT_CONFIG =
+        new ConfigSection()
             .with("servers", new ConfigSection())
             .with("packs", new ConfigSection())
             .with("routes", new ConfigSection())
             .with("global", new ConfigList());
-    private static final Logger log = LoggerFactory.getLogger(PackServerPlugin.class);
-    private static final Component DEFAULT_KICK_MESSAGE = new ImmutableComponent(new Content.Translate("multiplayer.requiredTexturePrompt.disconnect"), Style.EMPTY, Collections.emptyList());
-    private static final Serializer<URI> URI_SERIALIZER = Serializer.STRING.flatMap(URI::toString, URI::create);
+    private static final Logger log =
+        LoggerFactory.getLogger(PackServerPlugin.class);
+    private static final Component DEFAULT_KICK_MESSAGE =
+        new ImmutableComponent(
+            new Content.Translate(
+                "multiplayer.requiredTexturePrompt.disconnect"),
+            Style.EMPTY, Collections.emptyList());
+    private static final Serializer<URI> URI_SERIALIZER =
+        Serializer.STRING.flatMap(URI::toString, URI::create);
 
     private FileWrapper<ConfigObject> config;
 
@@ -48,13 +53,12 @@ public class PackServerPlugin implements Plugin {
     private Map<String, List<ResourcePack>> routePacks;
     private List<ResourcePack> globalPacks;
 
-    public PackServerPlugin() {
-
-    }
+    public PackServerPlugin() {}
 
     @Nullable
     public URI getServer(String server) {
-        if(server == null) server = "default";
+        if (server == null)
+            server = "default";
         return servers.get(server);
     }
 
@@ -64,95 +68,116 @@ public class PackServerPlugin implements Plugin {
         ConfigSection root = config.getRoot().asSection();
 
         Registry<String, PackEntry> packs = Registry.createStringRegistry();
-        PackEntry.SERIALIZER.mapOf().deserialize(ConfigContext.INSTANCE, root.getSection("packs")).getOrThrow().forEach(packs::register);
+        PackEntry.SERIALIZER.mapOf()
+            .deserialize(ConfigContext.INSTANCE, root.getSection("packs"))
+            .getOrThrow()
+            .forEach(packs::register);
 
-        servers = URI_SERIALIZER.mapOf().deserialize(ConfigContext.INSTANCE, root.getSection("servers")).getOrThrow();
+        servers =
+            URI_SERIALIZER.mapOf()
+                .deserialize(ConfigContext.INSTANCE, root.getSection("servers"))
+                .getOrThrow();
 
         kickMessages = new HashMap<>();
-        for(PackEntry ent : packs.values()) {
-            if(ent.required()) {
+        for (PackEntry ent : packs.values()) {
+            if (ent.required()) {
                 Component kickMessage = ent.kickMessage();
-                if(kickMessage == null) {
+                if (kickMessage == null) {
                     kickMessage = DEFAULT_KICK_MESSAGE;
                 }
                 kickMessages.put(ent.uuid(), kickMessage);
             }
         }
 
-        try(ExecutorService svc = Executors.newCachedThreadPool()) {
+        Map<String, ResourcePack> finalPacks = new ConcurrentHashMap<>();
+        for (int i = 0; i < packs.getSize(); i++) {
+            String id = packs.idAtIndex(i);
+            PackEntry pack = packs.valueAtIndex(i);
 
-            Map<String, ResourcePack> finalPacks = new ConcurrentHashMap<>();
-            CompletableFuture<?>[] futures = new CompletableFuture[packs.getSize()];
-            for(int i = 0; i < futures.length; i++) {
-                String id = packs.idAtIndex(i);
-                PackEntry pack = packs.valueAtIndex(i);
-
-                futures[i] = pack.toPack(this, svc).thenAccept(rp -> finalPacks.put(id, rp));
-            }
-
-            CompletableFuture.allOf(futures).join();
-            InlineSerializer<ResourcePack> keySerializer = InlineSerializer.of(rp -> "", finalPacks::get);
-
-            routePacks = keySerializer.listOf().mapToList().mapOf().deserialize(ConfigContext.INSTANCE, root.getSection("routes")).getOrThrow();
-            globalPacks = keySerializer.listOf().mapToList().deserialize(ConfigContext.INSTANCE, root.getList("global")).getOrThrow();
+            finalPacks.put(id, pack.toPack(this));
         }
+
+        InlineSerializer<ResourcePack> keySerializer =
+            InlineSerializer.of(rp -> "", finalPacks::get);
+
+        routePacks =
+            keySerializer.listOf()
+                .mapToList()
+                .mapOf()
+                .deserialize(ConfigContext.INSTANCE, root.getSection("routes"))
+                .getOrThrow();
+        globalPacks =
+            keySerializer.listOf()
+                .mapToList()
+                .deserialize(ConfigContext.INSTANCE, root.getList("global"))
+                .getOrThrow();
     }
 
     @Override
     public void initialize(Proxy proxy) {
 
-
-        Path configFolder = proxy.getPluginManager().configFolder().resolve("pack_server");
-        try { Files.createDirectories(configFolder); } catch (IOException e) {
-            throw new RuntimeException("Could not create messenger directory", e);
+        Path configFolder =
+            proxy.getPluginManager().configFolder().resolve("pack_server");
+        try {
+            Files.createDirectories(configFolder);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create messenger directory",
+                                       e);
         }
 
-        this.config = proxy.fileCodecRegistry().findOrCreate(ConfigContext.INSTANCE, "config", configFolder, DEFAULT_CONFIG);
+        this.config = proxy.fileCodecRegistry().findOrCreate(
+            ConfigContext.INSTANCE, "config", configFolder, DEFAULT_CONFIG);
         config.save();
 
         reload();
         proxy.getCommands().register("ps", (sender, args) -> {
-            if(args.length == 2 && args[1].equals("reload")) {
+            if (args.length == 2 && args[1].equals("reload")) {
                 reload();
                 sender.sendMessage("Reloaded resource packs");
             }
         });
 
         proxy.clientConnectEvent().register(this, client -> {
-
-            if(client.getIntent() == ServerboundHandshakePacket.Intent.STATUS) return;
+            if (client.getIntent() == ServerboundHandshakePacket.Intent.STATUS)
+                return;
             client.preConnectBackendEvent().register(this, ev -> {
-
-                if(ev.p2.wasReconnected()) {
+                if (ev.p2.wasReconnected()) {
                     return;
                 }
 
-                if(!ev.p2.authenticated()) {
-                    log.info("Player {} is not authenticated!", ev.p2.username());
+                if (!ev.p2.authenticated()) {
+                    log.info("Player {} is not authenticated!",
+                             ev.p2.username());
                     return;
                 }
 
                 List<CompletableFuture<?>> futures = new ArrayList<>();
-                for(ResourcePack pack : globalPacks) {
-                    futures.add(ev.p2.sendResourcePack(pack).thenAccept(pck -> onComplete(client, pck)));
+                for (ResourcePack pack : globalPacks) {
+                    futures.add(ev.p2.sendResourcePack(pack).thenAccept(
+                        pck -> onComplete(client, pck)));
                 }
 
                 String id = proxy.getBackends().getId(ev.p1);
-                if(id != null && routePacks.containsKey(id)) {
-                    for(ResourcePack pack : routePacks.get(id)) {
-                        futures.add(ev.p2.sendResourcePack(pack).thenAccept(pck -> onComplete(client, pck)));
+                if (id != null && routePacks.containsKey(id)) {
+                    for (ResourcePack pack : routePacks.get(id)) {
+                        futures.add(ev.p2.sendResourcePack(pack).thenAccept(
+                            pck -> onComplete(client, pck)));
                     }
                 }
 
-                CompletableFuture.allOf(futures.toArray(CompletableFuture<?>[]::new)).join();
+                CompletableFuture
+                    .allOf(futures.toArray(CompletableFuture<?>[] ::new))
+                    .join();
             });
         });
     }
 
-    private void onComplete(ClientConnection conn, ServerboundResourcePackStatusPacket packet) {
-        if(packet.action() == ServerboundResourcePackStatusPacket.Action.DECLINED && kickMessages.containsKey(packet.packId())) {
+    private void onComplete(ClientConnection conn,
+                            ServerboundResourcePackStatusPacket packet) {
+        if (packet.action() ==
+                ServerboundResourcePackStatusPacket.Action.DECLINED &&
+            kickMessages.containsKey(packet.packId())) {
             conn.disconnect(kickMessages.get(packet.packId()));
         }
     }
-
 }
